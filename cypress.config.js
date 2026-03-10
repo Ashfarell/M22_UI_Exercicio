0506ac4d0bd7b70f7fe589a52cd3ebe71fa9a4df
Ashfarell <ashfarell@gmail.com>
2026-03-09 22:13:58 -0300

    add cypress test reports page

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true
+  },
+
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br",
     viewportWidth: 1280,
