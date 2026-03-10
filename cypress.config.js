6b18ec72e69bf6707534388d96d370d3dec6ce5b
Ashfarell <ashfarell@gmail.com>
2026-03-09 21:39:49 -0300

    cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br",
+    pageLoadTimeout: 120000,
     viewportWidth: 1280,
     viewportHeight: 720
   }
